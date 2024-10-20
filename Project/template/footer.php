0a40fa5d783558909659c7af77adae27aeefd8ad
Mike Jun Yang Chiu Lim <156245687+Mikejunyang@users.noreply.github.com>
2024-10-20 16:32:30 -0400

    Add files via upload

---
+++ Project/template/footer.php
@@ -0,0 +1,16 @@
+<!-- footer.php -->
+<footer class="bg-primary text-white text-center py-3">
+    <div class="container">
+        <div class="copyright">
+            © 2024 Children's Game Portal. All Rights Reserved.
+        </div>
+        <ul class="list-unstyled">
+            <li><strong>Mike Jun Yang Chiu Lim:</strong> Account registration and AJAX validation.</li>
+            <li><strong>Ba Minh Huy Tran:</strong> Account Sign-In/Out, database structure creation.</li>
+            <li><strong>ChangRu Jia:</strong> Game levels management and progress abandonment.</li>
+            <li><strong>WenTao Sun:</strong> Web page structure creation, game rounds history display.</li>
+            <li><strong>Darshini Pillay Vythilingum:</strong> GitHub management, integration of functionalities,
+                enhanced interactivity.</li>
+        </ul>
+    </div>
+</footer>
---
+++ Project/template/head.php
@@ -0,0 +1,11 @@
+<!-- head.php -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Children's Game Portal</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="../main.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
